e29e02540521cfa917b06b11124c731f314bc534
Griffin Arkilic <garkilic@gmail.com>
2022-05-19 10:54:15 -0700

    Added nav, main content, and contact info.

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,9 +3,9 @@ import "../style.css"
 
 export default function Main() {
     return (
-        <div className="container main">
-            <h1>Hello World</h1>
-            <p></p>
+        <div className="main section-container">
+            <h1 className="main--message">I'm <span className="highlight">Griffin Arkilic</span>, a front-end developer based in San Francisco, CA. I love building fantastic user experience and services that people need.</h1>
+            <p className="main--contact">👉 Get in touch <a href="mailto:garkilic@gmail.com" target="_blank" rel="noopener noreferrer">garkilic</a> </p>
         </div>
     )
 }
