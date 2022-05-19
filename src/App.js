1beb24279827b2fd780fc9f2a8927f52cf9572f5
Griffin Arkilic <garkilic@gmail.com>
2022-05-19 15:38:42 -0700

    Added new section and made minor design changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nav from './components/Nav'
 import Main from './components/Main'
+import Background from './components/Background';
 
 function App() {
   return (
     <div>
       <Nav />
       <Main />
+      <Background />
     </div>
   );
 }
---
+++ src/components/Background.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../style.css"
+
+export default function Background() {
+    return(
+        <div className="background section-container">
+            <h1><span className="highlight">Background</span></h1>
+            <p>For the past 5 years I have been working as a product manager helping companies build solutions that solve real problems.</p>
+            <p>I decided to transition in front-end development because, as much as I loved product management, I wanted to be the one doing the building.</p>
+            <a href="#">View my resume</a>
+        </div>
+    )
+}
