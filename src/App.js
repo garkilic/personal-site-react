3fbc0979d0eb9ca235c48891ed6cac61c35967a1
Griffin Arkilic <garkilic@gmail.com>
2022-05-19 10:19:29 -0700

    Organizing components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from 'react';
+import Main from './components/Main'
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "../style.css"
+
+export default function Main() {
+    return (
+        <div className="container main">
+            <h1>Hello World</h1>
+            <p></p>
+        </div>
+    )
+}
