00fa4d27401bf1743cf93cf9a2d5fe9870b93f62
Griffin Arkilic <garkilic@gmail.com>
2022-05-31 09:53:08 -0700

    Link changes and added a quote

--- src/components/Background.js
+++ src/components/Background.js
@@ -7,7 +7,8 @@ export default function Background() {
             <h1>Background</h1>
             <p>For the past 5 years I have been working as a product manager helping companies build solutions that solve real problems across iOS, aOS, and web.</p>
             <p>I decided to transition in front-end development because, as much as I loved product management, I wanted to be the one doing the building.</p>
-            <p ><a className="link--out" href="https://docs.google.com/document/d/15KFjXoAiiEoRO19QEp81EDZUZiWMAS3Wwqbe55vuAHg/edit?usp=sharing" target="_blank" rel="noopener noreferrer">View my resume <span>&#8594;</span>
+            <p>“You live and learn. At any rate, you live.” - Douglas Adams</p>
+            <p ><a className="link--out" href="https://griffin-arkilic-resume.netlify.app/" target="_blank" rel="noopener noreferrer">View my resume <span>&#8594;</span>
 </a></p>
         </div>
     )
