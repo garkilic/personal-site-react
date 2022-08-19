3e9aa8c01af2d053e1a763f7925f754df320a985
Griffin <garkilic@gmail.com>
2022-08-18 20:30:52 -0700

    Copy change

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,9 +5,17 @@ export default function Main() {
 	return (
 		<div className="section-container">
 			<h1 className="main--message">
-				I'm <span className="highlight">Griffin Arkilic</span>, a developer and product manager based in Oakland, CA. I love building
-				fantastic user experiences and amazing digital products.<br />Currently, I work on personal projects in web and game development as well as build/design/maintain websites for local small businesses at Black Sand Studios.
+				{" "}
+				I'm <span className="highlight">Griffin Arkilic</span>, a developer and
+				product manager based in Oakland, CA. I love building fantastic user
+				experiences and amazing digital products.
 			</h1>
+			<h1 className="main--message">
+				Currently, I work on personal projects in web and game development as
+				well as build/design/maintain websites for local small businesses at
+				Black Sand Studios.
+			</h1>
+
 			<p className="main--contact">
 				👉 Get in touch{" "}
 				<a
