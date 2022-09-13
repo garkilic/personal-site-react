bbb625883d1b081ee6507dbd21dd770a1fc42c90
Griffin Arkilic <59614723+garkilic@users.noreply.github.com>
2022-09-13 08:38:43 -0700

    Sizing + Copy changes

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,15 +6,11 @@ export default function Main() {
 		<div className="section-container">
 			<h1 className="main--message">
 				{" "}
-				I'm <span className="highlight">Griffin Arkilic</span>, a developer and
+				I'm <span className="highlight">Griffin Arkilic</span>, a web and game developer +
 				product manager based in Oakland, CA. I love building fantastic user
 				experiences and amazing digital products.
 			</h1>
-			<h1 className="main--message">
-				Currently, I work on personal projects in web and game development as
-				well as build/design/maintain websites for local small businesses at
-				Black Sand Studios.
-			</h1>
+			
 
 			<p className="main--contact">
 				👉 Get in touch{" "}
